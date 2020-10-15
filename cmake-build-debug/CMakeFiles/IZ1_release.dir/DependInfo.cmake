080b80892fcc18bdd37098c067cea6d20977a05e
artem <artemiygotovtsev@gmail.com>
2020-10-15 15:22:38 +0300

    project creation

---
+++ cmake-build-debug/CMakeFiles/IZ1_release.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/artem/CLionProjects/IZ1_release/list_functions.c" "/home/artem/CLionProjects/IZ1_release/cmake-build-debug/CMakeFiles/IZ1_release.dir/list_functions.c.o"
+  "/home/artem/CLionProjects/IZ1_release/ship_functions.c" "/home/artem/CLionProjects/IZ1_release/cmake-build-debug/CMakeFiles/IZ1_release.dir/ship_functions.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/artem/CLionProjects/IZ1_release/main.cpp" "/home/artem/CLionProjects/IZ1_release/cmake-build-debug/CMakeFiles/IZ1_release.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/IZ1_release.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IZ1_release.dir/list_functions.c.o"
+  "CMakeFiles/IZ1_release.dir/main.cpp.o"
+  "CMakeFiles/IZ1_release.dir/ship_functions.c.o"
+  "IZ1_release"
+  "IZ1_release.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/IZ1_release.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
